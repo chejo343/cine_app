5e5b121bde7c3fc6765ef87d63e2e1f25e20bb38
omar_codes <chejo343@gmail.com>
2020-08-27 15:13:42 -0600

    Reservas y boletos

--- api/boleto/models/boleto.js
+++ api/boleto/models/boleto.js
@@ -5,4 +5,13 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      const funcion = await strapi.query('funcion').model
+        .findById(result.reserva.funcion)
+      await strapi.query('pelicula').model
+        .findByIdAndUpdate(funcion.pelicula, {$inc: { taquilla: 1 }}, { new: true })
+    }
+  }
+};
