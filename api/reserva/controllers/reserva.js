5f36103eeb83445714879eca4f20fb5dda061379
omar_codes <chejo343@gmail.com>
2020-08-28 12:37:03 -0600

    Validacion de disponibilidad de boletos

--- api/reserva/controllers/reserva.js
+++ api/reserva/controllers/reserva.js
@@ -5,4 +5,20 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  validateBoletos: async (ctx) => {
+    const funcionID = ctx.request.body.funcion
+    const boletos = ctx.request.body.boletos
+    const reservas = await strapi.query('reserva').model
+      .find({ funcion: funcionID }).populate('boletos')
+    for (const reserva of reservas) {
+      for (const boleto of reserva.boletos) {
+        if (boletos.some(i => i.px === boleto.px && i.py === boleto.py)) {
+          ctx.response.status = 409
+          return ctx.body = { available: false }
+        }
+      }
+    }
+    ctx.send({ available: true })
+  }
+};
